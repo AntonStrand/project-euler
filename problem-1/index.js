2e149fbab8e67384c0b43adcb5deb534a451eb23
antonstrand <jag@antonstrand.se>
2018-09-19 15:27:30 +0200

    A point free solution

---
+++ problem-1/index.js
@@ -0,0 +1,3 @@
+const pointFree = require('./point-free')
+
+console.log('Point free:', pointFree(1000))
---
+++ problem-1/point-free.js
@@ -0,0 +1,46 @@
+const {
+  add,
+  always,
+  compose,
+  converge,
+  curry,
+  equals,
+  flip,
+  identity,
+  ifElse,
+  modulo,
+  or,
+  range,
+  reduce,
+  useWith,
+  __
+} = require('ramda')
+
+/* point free version - with ramda / Sanctuary */
+
+const unless = ifElse(__, identity, __)
+
+// :: Number -> Number -> Booleen
+const isMultiplesBy = curry(
+  compose(
+    equals(0),
+    flip(modulo)
+  )
+)
+
+// :: Number -> Boolean
+const isMultiplesBy3or5 = converge(or, [isMultiplesBy(5), isMultiplesBy(3)])
+
+// :: Number -> Number
+const getMultiplesBy3or5 = unless(isMultiplesBy3or5, always(0))
+
+// :: [Number] -> Number
+const sum = reduce(useWith(add, [identity, getMultiplesBy3or5]), 0)
+
+// :: Number -> Number
+const sumAllMultiplesTo = compose(
+  sum,
+  range(1)
+)
+
+module.exports = sumAllMultiplesTo
