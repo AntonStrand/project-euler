fd869d4ef064bdfb83fe4ee499e336cdba087929
antonstrand <jag@antonstrand.se>
2018-09-19 15:39:33 +0200

    Importing and showing result

--- problem-1/index.js
+++ problem-1/index.js
@@ -1,3 +1,14 @@
-const pointFree = require('./point-free')
+const pointFree = require('./src/point-free')
+const recursion = require('./src/recursion')
+
+/**
+ * Two solutions for Euler problem 1
+ * https://projecteuler.net/problem=1
+ *
+ * All of them are reducing the array to only have to iterate through it once.
+ *
+ * @author Anton Strand
+ */
 
 console.log('Point free:', pointFree(1000))
+console.log('Recursion :', recursion(1000))
