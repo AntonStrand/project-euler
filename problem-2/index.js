6d8f6a4b3b9b429ae85212917dcfcccdeaa716b5
antonstrand <jag@antonstrand.se>
2018-09-19 17:37:12 +0200

    Solution for problem 2

---
+++ problem-2/index.js
@@ -0,0 +1,25 @@
+/**
+ * Even Fibonacci numbers
+ * https://projecteuler.net/problem=2
+ *
+ * @author Anton Strand
+ */
+
+// last :: [a] -> a
+const last = xs => xs[xs.length - 1]
+
+// fibonacci :: Number -> [Number]
+const fibonacci = (maxTerm, current = 2, acc = [1]) =>
+  current <= maxTerm
+    ? fibonacci(maxTerm, current + last(acc), [...acc, current])
+    : acc
+
+// evenOrZero :: Number -> Number
+const evenOrZero = x => (x % 2 === 0 ? x : 0)
+
+// sumEven :: [Number] -> Number
+const sumEven = ([x, ...xs], acc = 0) =>
+  x !== undefined ? sumEven(xs, acc + evenOrZero(x)) : acc
+
+// RESULT
+console.log(sumEven(fibonacci(4000000)))
